69f4b185706e3c5245c02df5321475c4f004bbde
Robert Tate <btate712@gmail.com>
2019-02-27 12:53:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     output_string = "The line is currently: "
-    for person = 1..katz_deli.length
+    for person in 1..katz_deli.length
       output_string += "#{person}. #{katz_deli[person - 1]}"
     end
     puts output_string
